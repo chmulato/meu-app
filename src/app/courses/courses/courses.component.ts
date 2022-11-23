d3c3dd32104eb874e5f2c0527814a8dacc11f54b
Christian Mulato <chmulato@hotmail.com>
2022-11-23 11:04:56 -0300

    Utilizando o hhtClient do RxJs

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -8,14 +10,15 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id:'1', name: 'Angular', category: 'Front-end'}
-  ];
-  
+  courses: Observable<Course[]>;
   displayedColumns = ['name','category'];
 
-  constructor() { 
+  //coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) { 
     // this.courses = [];
+    // this.coursesService = new CoursesService();
+    this.courses = this.coursesService.list();
   }
 
   ngOnInit(): void {
