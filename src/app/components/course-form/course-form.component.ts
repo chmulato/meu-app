30b54749326a11704a73521d78f6aa1b0a1517f5
Christian Mulato <chmulato@hotmail.com>
2022-11-26 20:53:22 -0300

    Validações de Formulário no Front-end

--- src/app/components/course-form/course-form.component.ts
+++ src/app/components/course-form/course-form.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { CoursesService } from 'src/app/services/courses.service';
 import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { NonNullableFormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from 'src/app/model/course';
+
+import { CoursesService } from 'src/app/services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -15,8 +16,10 @@ export class CourseFormComponent implements OnInit {
 
   form = this.formBuilder.group({
     _id: [''],
-    name: [''],
-    category: ['']
+    name: ['', [Validators.required, 
+      Validators.minLength(5), 
+      Validators.maxLength(100)]],
+    category: ['', [Validators.required]]
   });
 
   constructor(
@@ -52,5 +55,24 @@ export class CourseFormComponent implements OnInit {
 
   private onError() {
     this.snackBar.open('Erro ao salvar curso!', '', { duration: 5000 });
+  }
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength: number = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres.`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength: number = field.errors ? field.errors['maxlength']['requiredLength'] : 100;
+      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
+    }
+
+    return 'Campo Inválido';
   }
 }
